5329cbc35f2cd3573d23cf4535d8cab3fc91fa47
etgcrog <61814067+etgcrog@users.noreply.github.com>
2020-05-11 10:04:34 -0300

    projetos_brasicos

---
+++ jogo_advinhar.py
@@ -0,0 +1,36 @@
+from random import randint
+from time import sleep
+class JogoAdvinhar:
+    def __init__(self):
+        self.acertou = False
+
+    def Iniciar(self):
+        sleep(1)
+        print('Acabei de pensar em número de 1 até 10. Tente acertar!!!')
+        self.ValorDoComputador()
+        tentativas = 0
+        try:
+            while not self.acertou:
+                self.ValorDoJogador()
+                tentativas += 1
+                if self.computador == self.jogador:
+                    print('Você ACERTOU, PARABÉNS')
+                    self.acertou = True
+                else:
+                    if self.jogador < self.computador:
+                        print('Tente um número maior...')
+                    elif self.jogador > self.computador:
+                        print('Tente um número menor...')
+            print(f'Acertou com {tentativas}. Parabéns e volte sempre!')
+        except:
+            print('Por favor, digite apenas númeoros inteiros.')
+            self.Iniciar()
+
+    def ValorDoJogador(self):
+        self.jogador = int(input('Qual é o seu palpite? '))
+
+    def ValorDoComputador(self):
+        self.computador = randint(1,100)
+
+jogar = JogoAdvinhar()
+jogar.Iniciar()
--- simulador-de-dados.py
+++ simulador-de-dados.py
@@ -1,27 +1,30 @@
 # Simulador de dado
 # Simular o uso de um dado,gerando um valor de 1 até 6
 from random import randint
+import PySimpleGUI as sg
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Você gostaria de gerar um novo valor para o dado?[Sim/Não]:'
+        #layout
+        self.layout = [
+            [sg.Text('Quer lançar o dado?',size=(15,0))],
+            [sg.Button('SIM',size=(5,0)),sg.Button('NAO',size=(5,0))]
+        ]
 
     def Iniciar(self):
-        while True:
-            resp = input(self.mensagem)
-            if resp == 'sim' or resp == 's':
-                self.GerarValorDoDado()
-                continuar = str(input("Quer continuar?[S/n]"))
-                if continuar == 's' or continuar == 'sim':
-                    continue
-                else:
-                    print('Saindo do simulador...')
-                    break
-            elif resp == 'nao' or resp == 'n':
-                print('Agrademos sua participação!')
-                break
+        # Janela
+        self.janela = sg.Window('Simulador de Dado',layout=self.layout)
+        # Ler os Dados
+        self.eventos, self.valores = self.janela.Read()
+        # Fazer o iniciar
+
+        if self.eventos == 'SIM' or self.eventos == 's':
+            self.GerarValorDoDado()
+            continuar = str(input("Quer continuar?[S/n]"))
+        elif self.eventos == 'NAO' or self.eventos == 'n':
+            print('Agrademos sua participação!')
 
 
 
@@ -31,4 +34,3 @@ class SimuladorDeDado:
 
 simulador = SimuladorDeDado()
 simulador.Iniciar()
-
